c48d75bd51f1234f9b1807801ba3c2ab6144c3fb
LsrkMateo <mmateoguillen@gmail.com>
2023-08-30 18:11:38 -0500

    Add dark mode and more information

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,11 +2,14 @@
 
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useDarkMode } from "./context";
+import { format } from "date-fns";
 
 function Page() {
+  const { darkMode, toggleDarkMode } = useDarkMode();
   const linkArray = [
     "https://api.github.com/repos/LsrkMateo/next-js-mongodb",
-    "https://api.github.com/repos/LsrkMateo/Hashnode-blogs"
+    "https://api.github.com/repos/LsrkMateo/Hashnode-blogs",
   ];
 
   const [repoData, setRepoData] = useState([]);
@@ -23,22 +26,60 @@ function Page() {
   useEffect(() => {
     linkArray.forEach((link) => getRepo(link));
   }, []);
-
+  console.log(repoData);
   return (
-    <div className="bg-gray-100 min-h-screen p-8">
+    <div
+      className={`min-h-screen p-8 ${darkMode ? "bg-gray-950" : "bg-gray-100"}`}
+    >
       <div className="grid grid-cols-2 gap-4">
         {repoData.length > 0 ? (
           repoData.map((data, index) => (
-            <div key={index} className="bg-white p-4 rounded shadow-lg">
-              <div className="text-2xl font-bold mb-4">Nombre: {data.name}</div>
-              <div className="text-gray-700 mb-2">
+            <div
+              key={index}
+              className={`p-4 rounded shadow-lg ${
+                darkMode ? "bg-gray-800 text-white" : "bg-white"
+              }`}
+            >
+              <div
+                className={`text-2xl font-bold mb-4 ${
+                  darkMode ? "text-white" : "text-gray-900"
+                }`}
+              >
+                Nombre: {data.name}
+              </div>
+              <div
+                className={`mb-2 ${
+                  darkMode ? "text-gray-400" : "text-gray-700"
+                }`}
+              >
                 Autor: {data.owner.login}
               </div>
+              <div
+                className={`mb-2 ${
+                  darkMode ? "text-gray-400" : "text-gray-700"
+                }`}
+              >
+                Description: {!data.description ? "no tiene" : data.description}
+              </div>
+              <div
+                className={`mb-2 ${
+                  darkMode ? "text-gray-400" : "text-gray-700"
+                }`}
+              >
+                Última actualización:  {format(new Date(data.pushed_at), "dd/MM/yyyy HH:mm")}
+              </div>
+
               {/* Resto de los detalles del repositorio */}
             </div>
           ))
         ) : (
-          <div className="text-center text-gray-700">Cargando...</div>
+          <div
+            className={`text-center ${
+              darkMode ? "text-gray-400" : "text-gray-700"
+            }`}
+          >
+            Cargando...
+          </div>
         )}
       </div>
     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 
-function NavBar() {
+function NavBar({ dark }) {
   return (
-    <nav className="bg-gray-950 py-5 mb-2">
+    <nav className={`py-5 mb-2 ${dark ? "bg-black" : "bg-gray-200"}`}>
       <div className="container mx-auto flex items-center justify-center">
         <Link href="/">
-          <h1 className="text-2xl font-bold">Repositorio de proyectos</h1>
+          <h1 className={`text-2xl font-bold ${dark ? "text-white" : "text-gray-900"}`}>
+            Repositorio de proyectos
+          </h1>
         </Link>
       </div>
     </nav>
